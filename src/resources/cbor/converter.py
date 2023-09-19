312c274233171a7b9405101d2d2d7b7bd2f957c0
Alex Huang Feng <alex.huang-feng@insa-lyon.fr>
2023-09-19 17:28:36 +0200

    feat: convert all files instead of mannually convert one by one

--- src/resources/cbor/converter.py
+++ src/resources/cbor/converter.py
@@ -1,101 +1,23 @@
 import cbor2
+import glob
 import json
+from os.path import basename
 
+CBOR_dir_output = "./notifications/"
 
-############ subscription-started ############
-subscription_started_json = None
-with open("../json/notifications/subscription-started.json", 'r') as f:
-  subscription_started_json = json.load(f)
+json_notifications = [f for f in glob.glob("../json/notifications/*.json")]
 
-subscription_started_cbor = cbor2.dumps(subscription_started_json)
+print(json_notifications)
 
-with open('./notifications/subscription-started.cbor', 'wb') as fp:
-  cbor2.dump(subscription_started_cbor, fp)
+for json_notification_path in json_notifications:
+  print("Converting " + json_notification_path)
 
-############ subscription-started_with_anchor-time ############
-subscription_started_with_anchor_time_json = None
-with open("../json/notifications/subscription-started_with_anchor-time.json", 'r') as f:
-  subscription_started_with_anchor_time_json = json.load(f)
+  json_notification = None
+  with open(json_notification_path, 'r') as f:
+    json_notification = json.load(f)
+  
+  cbor_notification = cbor2.dumps(json_notification)
+  output_cbor = CBOR_dir_output + basename(json_notification_path).replace('json', 'cbor')
 
-subscription_started_with_anchor_time_cbor = cbor2.dumps(subscription_started_with_anchor_time_json)
-
-with open('./notifications/subscription-started_with_anchor-time.cbor', 'wb') as fp:
-  cbor2.dump(subscription_started_with_anchor_time_cbor, fp)
-
-
-############ subscription-modified ############
-subscription_modified_json = None
-with open("../json/notifications/subscription-modified.json", 'r') as f:
-  subscription_modified_json = json.load(f)
-
-subscription_modified_cbor = cbor2.dumps(subscription_modified_json)
-
-with open('./notifications/subscription-modified.cbor', 'wb') as fp:
-  cbor2.dump(subscription_modified_cbor, fp)
-
-
-############ subscription-terminated ############
-subscription_terminated_json = None
-with open("../json/notifications/subscription-terminated.json", 'r') as f:
-  subscription_terminated_json = json.load(f)
-
-subscription_terminated_cbor = cbor2.dumps(subscription_terminated_json)
-
-with open('./notifications/subscription-terminated.cbor', 'wb') as fp:
-  cbor2.dump(subscription_terminated_cbor, fp)
-
-
-############ push-update-1 ############
-push_update_1_json = None
-with open("../json/notifications/push-update-1.json", 'r') as f:
-  push_update_1_json = json.load(f)
-
-push_update_1_cbor = cbor2.dumps(push_update_1_json)
-
-with open('./notifications/push-update-1.cbor', 'wb') as fp:
-  cbor2.dump(push_update_1_cbor, fp)
-
-
-############ push-update-2 ############
-push_update_2_json = None
-with open("../json/notifications/push-update-2.json", 'r') as f:
-  push_update_2_json = json.load(f)
-
-push_update_2_cbor = cbor2.dumps(push_update_2_json)
-
-with open('./notifications/push-update-2.cbor', 'wb') as fp:
-  cbor2.dump(push_update_2_cbor, fp)
-
-
-############ push-update-subscription ############
-push_update_subscription_json = None
-with open("../json/notifications/push-update-subscription.json", 'r') as f:
-  push_update_subscription_json = json.load(f)
-
-push_update_subscription_cbor = cbor2.dumps(push_update_subscription_json)
-
-with open('./notifications/push-update-subscription.cbor', 'wb') as fp:
-  cbor2.dump(push_update_subscription_cbor, fp)
-
-
-############ push-update-subscription ############
-push_update_subscription_json = None
-with open("../json/notifications/push-update-subscription.json", 'r') as f:
-  push_update_subscription_json = json.load(f)
-
-push_update_subscription_cbor = cbor2.dumps(push_update_subscription_json)
-
-with open('./notifications/push-update-subscription.cbor', 'wb') as fp:
-  cbor2.dump(push_update_subscription_cbor, fp)
-
-
-############ push-change-update-subscription ############
-push_change_update_subscription_json = None
-with open("../json/notifications/push-change-update-subscription.json", 'r') as f:
-  push_change_update_subscription_json = json.load(f)
-
-push_change_update_subscription_cbor = cbor2.dumps(push_change_update_subscription_json)
-
-with open('./notifications/push-change-update-subscription.cbor', 'wb') as fp:
-  cbor2.dump(push_change_update_subscription_cbor, fp)
-
+  with open(output_cbor, 'wb') as fp:
+    cbor2.dump(cbor_notification, fp)
